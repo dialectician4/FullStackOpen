187b286a6cf16da5664a93d7100f978b8663bb0f
dialectician4 <edwin.santos.kov@gmail.com>
2025-05-22 21:12:33 -0400

    Integrating notes backend and frontend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+//const cors = require('cors');
+//app.use(cors());
+app.use(express.static('dist'));
 
 
-let notes = [  
-	{    
-		id: "1",    
-		content: "HTML is easy",    
-		important: true  
-	},  
-	{    
-		id: "2",    
-		content: "Browser can execute only JavaScript",    
-		important: false  
-	},  
-	{    
-		id: "3",    
-		content: "GET and POST are the most important methods of HTTP protocol",    
-		important: true  
+let notes = [
+	{
+		id: "1",
+		content: "HTML is easy",
+		important: true
+	},
+	{
+		id: "2",
+		content: "Browser can execute only JavaScript",
+		important: false
+	},
+	{
+		id: "3",
+		content: "GET and POST are the most important methods of HTTP protocol",
+		important: true
 	}
 ]
 
@@ -48,16 +51,16 @@ app.delete('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
 	const maxId = notes.length > 0
-	? Math.max(...notes.map(n => Number(n.id)))
-	:0;
-	return String(maxId +1)
+		? Math.max(...notes.map(n => Number(n.id)))
+		: 0;
+	return String(maxId + 1)
 };
 
 app.post('/api/notes', (request, response) => {
 	// const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
 	const body = request.body;
 	if (!body.content) {
-		return response.status(400).json({error: 'content missing'})
+		return response.status(400).json({ error: 'content missing' })
 	}
 	const note = {
 		content: body.content,
