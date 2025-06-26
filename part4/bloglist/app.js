e6c1ff8c4c35a61813633bdb73ec6fc1541a1894
dialectician4 <edwin.santos.kov@gmail.com>
2025-06-25 22:32:38 -0400

    Adding bcrypt dependency

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,26 +1,28 @@
-require('express-async-errors');
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-const blogsRouter = require('./controllers/blogs');
+require("express-async-errors");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config.js");
+const logger = require("./utils/logger.js");
+const middleware = require("./utils/middleware.js");
+const blogsRouter = require("./controllers/blogs.js");
+const usersRouter = require("./controllers/user.js");
 
 const app = express();
 
-logger.info('connecting to', config.MONGODB_URI);
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
-  .then(() => logger.info('connected to MongoDB'))
+  .then(() => logger.info("connected to MongoDB"))
   .catch((error) =>
-    logger.error('error connection to MongoDB:', error.message)
+    logger.error("error connection to MongoDB:", error.message)
   );
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
-app.use('/api/blogs', blogsRouter);
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
